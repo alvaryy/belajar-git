f81a4d6174d00a1bcb205cdd2f5cfa5235701171
alvaryy <alviarya11@gmail.com>
2025-02-19 14:21:57 +0700

    Delete Bab3_Function.dart

--- Bab3_Function.dart
+++
@@ -1,9 +0,0 @@
-// Menulis fungsi di luar fungsi main.
-void cetakNama() {
-  print("Nama saya Alvi Arya Noviansyah. Saya Sekolah di SMK Telkom Jakarta.");
-}
-
-// Ini adalah fungsi utama kita.
-void main() {
-  cetakNama();
-}
